9f12d45aad7c18fec1e134fbef34bdbf2d4d1d4f
KKSotirov <sotirov.k73@gmail.com>
2026-08-06 22:46:26 +0300

    Second commit for final boss for exam prep [Strings] -> Added numerous individual functions to help normalize the string

--- task_6.cpp
+++ task_6.cpp
@@ -84,14 +84,113 @@ void removeEdgeIntervals(char *&str)
     }
     helperContainer -= newLen;
     char *newStr = new char[newLen + 1];
-    for (size_t i = 0; i < newLen; i++)
-    {
-        newStr[i] = helperContainer[i];
-    }
-    newStr[len] = '\0';
+    strcpy(newStr, helperContainer);
     delete[] helperContainer;
     delete[] str;
     str = newStr;
+}
+
+void compressIntervals(char *&str)
+{
+    unsigned len = strlen(str);
+    const char *ptr = str;
+    char *helperContainer = new char[len + 1];
+    unsigned newLen = 0;
+    while (*ptr)
+    {
+        if (*ptr == ' ')
+        {
+            while (*ptr && *ptr == ' ')
+            {
+                ptr++;
+            }
+            *helperContainer = ' ';
+            // helperContainer++;
+            // newLen++;
+        }
+        else
+        {
+            *helperContainer = *ptr;
+            // helperContainer++;
+            // newLen++;
+        }
+        helperContainer++;
+        newLen++;
+        ptr++;
+    }
+    helperContainer -= newLen;
+    char *newStr = new char[newLen + 1];
+    strcpy(newStr, helperContainer);
+    delete[] helperContainer;
+    delete[] str;
+    str = newStr;
+}
+
+bool isSentenceEnd(const char ch)
+{
+    return (ch == '.' || ch == '!' || ch == '?');
+}
+
+void cutConsecutivePunctuationMarks(char *&str)
+{
+    unsigned len = strlen(str);
+    const char *ptr = str;
+    char *helperContainer = new char[len + 1];
+    unsigned newLen = 0;
+    while (*ptr)
+    {
+        if (isSentenceEnd(*ptr))
+        {
+            while (*ptr && isSentenceEnd(*ptr))
+            {
+                ptr++;
+            }
+            *helperContainer = *(ptr - 1); // last consecutive punctuation mark
+        }
+        else
+        {
+            *helperContainer = *ptr;
+        }
+        helperContainer++;
+        newLen++;
+        ptr++;
+    }
+}
+
+void transformIntoSentences(char *&str)
+{
+    bool isSentenceStart = true;
+    unsigned len = strlen(str);
+    char *ptr = str;
+    char *helperContainer = new char[len + 1];
+    unsigned newLen = 0;
+    // this function : 1. transforms letters to capital and lower
+    while (*ptr)
+    {
+        if (isSentenceStart && isLetter(*ptr))
+        {
+            *helperContainer = *ptr;
+            toCapitalCase(*helperContainer);
+            helperContainer++;
+            ptr++;
+            isSentenceStart = false;
+            newLen++;
+        }
+        if (isSentenceEnd(*ptr))
+        {
+            isSentenceStart = true;
+            cutConsecutivePunctuationMarks(ptr);
+        }
+
+        if (isSentenceStart == false && isLetter(*ptr))
+        {
+            *helperContainer = *ptr;
+            toLowerCase(*helperContainer);
+            helperContainer++;
+            ptr++;
+            newLen++;
+        }
+    }
 }
 
 int main()
